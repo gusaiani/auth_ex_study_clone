54dcbe9e512b961daca15637564e58db909ee563
Gustavo Saiani <gustavo@poe.ma>
2017-11-30 11:37:28 -0200

    Install dependencies

---
+++ lib/auth_ex/auth/user.ex
@@ -0,0 +1,20 @@
+defmodule AuthEx.Auth.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias AuthEx.Auth.User
+
+
+  schema "users" do
+    field :password, :string
+    field :username, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(%User{} = user, attrs) do
+    user
+    |> cast(attrs, [:username, :password])
+    |> validate_required([:username, :password])
+  end
+end
